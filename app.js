9b2a4d73b8af7ede55dc892db338b0932395c46e
Azie Melza Pratama <aziemp66@gmail.com>
2022-02-27 22:33:26 +0700

    Adding Some Endpoints

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/database');
+const db = require("./data/database");
 
 const app = express();
 
 app.use(function (error, req, res, next) {
-  res.status(500).json({
-    message: 'Something went wrong!',
-  });
+    res.status(500).json({
+        message: "Something went wrong!",
+    });
 });
 
 db.initDb()
-  .then(function () {
-    app.listen(3000);
-  })
-  .catch(function (error) {
-    console.log('Connecting to the database failed!');
-  });
+    .then(function () {
+        app.listen(3000);
+    })
+    .catch(function (error) {
+        console.log("Connecting to the database failed!");
+    });
---
+++ controllers/todo.controller.js
@@ -0,0 +1,47 @@
+const Todo = require("../models/todo.model");
+
+async function getAllTodos(req, res, next) {
+    let todos;
+    try {
+        todos = await Todo.getAllTodos();
+    } catch (error) {
+        return next(error);
+    }
+
+    res.json({
+        todos: todos,
+    });
+}
+
+async function addTodo(req, res, next) {
+    const todoText = req.body.text;
+
+    const todo = new Todo(todoText);
+
+    let insertedId;
+    try {
+        const result = await todo.save();
+
+        insertedId = result.insertedId;
+    } catch (error) {
+        return next(error);
+    }
+
+    todo.id = insertedId.toString();
+
+    res.json({
+        message: "Todo added successfully!",
+        createdTodo: todo,
+    });
+}
+
+function updateTodo(req, res, next) {}
+
+function deleteTodo(req, res, next) {}
+
+module.exports = {
+    getAllTodos,
+    addTodo,
+    updateTodo,
+    deleteTodo,
+};
